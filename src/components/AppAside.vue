2243017aa2bedc1074679382a7fd32043e91bc93
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-02-14 15:16:56 +0500

    feat: add basic aside layout

--- src/components/AppAside.vue
+++ src/components/AppAside.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    <h1>App Aside</h1>
-  </div>
+  <aside class="aside">
+    <div class="aside-header"></div>
+
+    <div class="aside-body">
+      <div class="aside-body__top">
+        <img class="aside-icon" src="" alt="Weather icon" />
+        <p class="aside-temperature">12</p>
+        <p class="aside-text">
+          <span class="aside-text__day">Monday</span>,
+          <span class="aside-text__time">16:00</span>
+        </p>
+      </div>
+
+      <div class="aside-body__bottom">
+        <div class="condition">
+          <img class="condition-icon" src="" alt="Condition icon" />
+          <p class="condition-text">Mostly cloudy</p>
+        </div>
+        <div class="condition">
+          <img class="condition-icon" src="" alt="Condition icon" />
+          <p class="condition-text">Rain - 30%</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="aside-footer">
+      <p class="aside-city">Tashkent</p>
+    </div>
+  </aside>
 </template>
