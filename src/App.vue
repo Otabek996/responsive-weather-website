7e83b725223e54747697e06a582e1c900b1877ce
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-02-14 15:34:23 +0500

    feat: add variables

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,26 @@ fetchData();
 /*==================== GOOGLE FONTS ====================*/
 @import url("https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap");
 
-/*==================== VARIABLES CSS ====================*/
+/*==================== VARIABLES ====================*/
+:root {
+  /*========== Colors ==========*/
+  --body-background-color-white: #ffffff;
+  --body-background-color-gray: #f6f6f8;
+  --body-background-color-grayer: #d6d7da;
+  --text-color-black: #000000;
+  --text-color-gray: #aeaeae;
+  --active-btn-background-color-black: #000000;
+
+  /*========== Font and typography ==========*/
+  --font-family-inconsolata: "Inconsolata", sans-serif;
+
+  /*========== Font size ==========*/
+  /*========== Line height ==========*/
+  /*========== Font weight ==========*/
+  --font-weight-regular: 400;
+  --font-weight-bold: 700;
+}
+
 /*==================== BASE ====================*/
 * {
   box-sizing: border-box;
