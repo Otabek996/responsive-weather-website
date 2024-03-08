ebf1c1f0d790230edb31a4a45396509e7d9dd723
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-03-08 23:47:14 +0500

    feat: add error ctahc

--- src/App.vue
+++ src/App.vue
@@ -113,6 +113,9 @@ export default {
           this.forecastDays = res.data.forecast.forecastday;
 
           console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
         });
     },
 
