e8b2abad2dc6ad90cbed872b3558a7e46e625c56
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-03-12 00:02:54 +0500

    feat: add vuetify library

--- src/main.js
+++ src/main.js
@@ -2,4 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "remixicon/fonts/remixicon.css";
 
-createApp(App).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
